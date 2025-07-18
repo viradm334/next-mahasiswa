060041e44de452f4a4952cd70654f32279160a7f
viradm334 <viradamayanti@student.telkomuniversity.ac.id>
2025-07-18 13:56:31 +0700

    frontend create mahasiswa

--- src/app/page.js
+++ src/app/page.js
@@ -1,35 +1,35 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { useRouter } from 'next/navigation';
+import { useRouter } from "next/navigation";
+import Link from "next/link";
 
 export default function Home() {
   const [message, setMessage] = useState("");
   const [users, setUsers] = useState([]);
   const router = useRouter();
 
-  async function handleDelete(id){
-    const confirmed = confirm('Anda yakin menghapus mahasiswa?');
+  async function handleDelete(id) {
+    const confirmed = confirm("Anda yakin menghapus mahasiswa?");
 
-    if(!confirmed) return;
+    if (!confirmed) return;
 
-    try{
+    try {
       const res = await fetch(`/api/delete-user/${id}`, {
-        method: 'DELETE'
+        method: "DELETE",
       });
 
-      if(res.ok){
-        setMessage('Berhasil delete mahasiswa!');
-        setUsers(users.filter(user => user.id !== id));
-      }else{
+      if (res.ok) {
+        setMessage("Berhasil delete mahasiswa!");
+        setUsers(users.filter((user) => user.id !== id));
+      } else {
         const data = await res.json();
-        console.error('Gagal hapus mahasiswa', data.message);
+        console.error("Gagal hapus mahasiswa", data.message);
       }
-
-    }catch(err){
-      console.error('Error deleting user:', err);
+    } catch (err) {
+      console.error("Error deleting user:", err);
     }
-  };
+  }
 
   useEffect(() => {
     fetch("/api/get-users")
@@ -43,45 +43,64 @@ export default function Home() {
 
   return (
     <div>
-      <h4 className="text-center text-2xl font-bold">Daftar Mahasiswa</h4>
+      <h4 className="text-center text-2xl font-bold mt-3">Daftar Mahasiswa</h4>
 
       <div className="container p-10 flex flex-col justify-center items-center">
-      <button type="button" className="py-1.5 px-3 mb-3 bg-green-500 hover:bg-green-700 text-white rounded-md cursor-pointer">Tambah Mahasiswa</button>
-      <table className="border-collapse border border-gray-400">
-        <thead>
-          <tr>
-            <th className="border border-gray-300 p-2">No.</th>
-            <th className="border border-gray-300 p-2">Nama</th>
-            <th className="border border-gray-300 p-2">NIM</th>
-            <th className="border border-gray-300 p-2">Email</th>
-            <th className="border border-gray-300 p-2">Jurusan</th>
-            <th className="border border-gray-300 p-2">Action</th>
-          </tr>
-        </thead>
-        <tbody>
-          {users.length === 0 ? (
+        <Link href="/create">
+          <button
+            type="button"
+            className="py-1.5 px-3 mb-3 bg-green-500 hover:bg-green-700 text-white rounded-md cursor-pointer"
+          >
+            Tambah Mahasiswa
+          </button>
+        </Link>
+        <table className="border-collapse border border-gray-400">
+          <thead>
             <tr>
-              <td colSpan="3" className="p-4 text-center">
-                Loading...
-              </td>
+              <th className="border border-gray-300 p-2">No.</th>
+              <th className="border border-gray-300 p-2">Nama</th>
+              <th className="border border-gray-300 p-2">NIM</th>
+              <th className="border border-gray-300 p-2">Email</th>
+              <th className="border border-gray-300 p-2">Jurusan</th>
+              <th className="border border-gray-300 p-2">Action</th>
             </tr>
-          ) : (
-            users.map((user, index) => (
-              <tr key={user.id}>
-                <td className="border border-gray-300 p-2">{index + 1}</td>
-                <td className="border border-gray-300 p-2">{user.name}</td>
-                <td className="border border-gray-300 p-2">{user.nim}</td>
-                <td className="border border-gray-300 p-2">{user.email}</td>
-                <td className="border border-gray-300 p-2">{user.jurusan}</td>
-                <td className="border border-gray-300 p-2">
-                  <button type="button" className="py-1.5 px-3 mr-2 bg-blue-500 hover:bg-blue-700 text-white rounded-md cursor-pointer">Edit</button>
-                  <button type="button" className="py-1.5 px-3 bg-red-500 hover:bg-red-700 text-white rounded-md cursor-pointer" value={user.id} onClick={() => handleDelete(user.id)}>Delete</button>
+          </thead>
+          <tbody>
+            {users.length === 0 ? (
+              <tr>
+                <td colSpan="3" className="p-4 text-center">
+                  Loading...
                 </td>
               </tr>
-            ))
-          )}
-        </tbody>
-      </table>
+            ) : (
+              users.map((user, index) => (
+                <tr key={user.id}>
+                  <td className="border border-gray-300 p-2">{index + 1}</td>
+                  <td className="border border-gray-300 p-2">{user.name}</td>
+                  <td className="border border-gray-300 p-2">{user.nim}</td>
+                  <td className="border border-gray-300 p-2">{user.email}</td>
+                  <td className="border border-gray-300 p-2">{user.jurusan}</td>
+                  <td className="border border-gray-300 p-2">
+                    <button
+                      type="button"
+                      className="py-1.5 px-3 mr-2 bg-blue-500 hover:bg-blue-700 text-white rounded-md cursor-pointer"
+                    >
+                      Edit
+                    </button>
+                    <button
+                      type="button"
+                      className="py-1.5 px-3 bg-red-500 hover:bg-red-700 text-white rounded-md cursor-pointer"
+                      value={user.id}
+                      onClick={() => handleDelete(user.id)}
+                    >
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              ))
+            )}
+          </tbody>
+        </table>
       </div>
     </div>
   );
